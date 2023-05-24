069ccacf903718adf16f9f4f2bbd2ce270c931a4
Flagelo6482 <frankln6482@gmail.com>
2023-05-24 16:28:54 -0700

    feat: File bubble :rocket:

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Bubble sort algorithm
+ *@array: Pointer to array
+ *@size: Array length
+ *
+ *Return: Void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t tmp, i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+			}
+		}
+		print_array(array, size);
+	}
+}
